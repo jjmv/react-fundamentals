f634e27c0470008b2defca3c7ebe91fb0c079abf
Javier Moreno <jesus.moreno@definityfirst.com>
2021-03-16 15:58:35 -0600

    Forms lesson

--- src/exercise/06.js
+++ src/exercise/06.js
@@ -8,6 +8,21 @@ function UsernameForm({onSubmitUsername}) {
   // 💰 Make sure to accept the `event` as an argument and call
   // `event.preventDefault()` to prevent the default behavior of form submit
   // events (which refreshes the page).
+
+  const inputRef = React.useRef()
+  const [value, sertValue] = React.useState()
+
+  const handleSubmit = e => {
+    e.preventDefault()
+    const value = inputRef.current.value
+    onSubmitUsername(value)
+  }
+
+  const handleChange = e => {
+    const valLower = e.target.value.toLowerCase()
+    // const isValid = value === value.toLowerCase()
+    sertValue(valLower)
+  }
   //
   // 🐨 get the value from the username input (using whichever method
   // you prefer from the options mentioned in the instructions)
@@ -19,10 +34,16 @@ function UsernameForm({onSubmitUsername}) {
   // 🐨 make sure to associate the label to the input.
   // to do so, set the value of 'htmlFor' prop of the label to the id of input
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <div>
-        <label>Username:</label>
-        <input type="text" />
+        <label htmlFor="username">Username:</label>
+        <input
+          onChange={handleChange}
+          ref={inputRef}
+          type="text"
+          id="username"
+          value={value}
+        />
       </div>
       <button type="submit">Submit</button>
     </form>
